81b21330f1262c51700f815dad6f1e22b282aeac
Mario Arturo sierra juarez <mario_asj@outlook.com>
2023-05-06 16:39:43 -0600

    Uso de While

---
+++ UsoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso While
+            int i;
+            Console.WriteLine("*******Ejemplo de While*******");
+            Console.WriteLine("Imprime numeros de 1 hasta 10");
+            i = 1; 
+            while (i < 11)
+            {
+                Console.WriteLine(i);
+                i++; 
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
